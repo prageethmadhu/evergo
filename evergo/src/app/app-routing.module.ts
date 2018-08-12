d40d93089e767ffbb57eafdc1ae95da7ab179d8e
prageeth <prageeth.madhu@gmail.com>
2018-08-12 08:54:30 +0530

    add routing

--- evergo/src/app/app-routing.module.ts
+++ evergo/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
- 
-];
+
 @NgModule({
-  
   imports: [
-    [ RouterModule.forRoot(routes) ]
+    CommonModule
   ],
   declarations: []
 })
