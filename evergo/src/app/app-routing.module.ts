05b5eb85fdb26b63abbe610aaa4fc5abea38c7da
prageeth <prageeth.madhu@gmail.com>
2018-08-12 10:09:51 +0530

    add images

--- evergo/src/app/app-routing.module.ts
+++ evergo/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [ RouterModule.forRoot(routes) ],
   declarations: []
 })
 export class AppRoutingModule { }
