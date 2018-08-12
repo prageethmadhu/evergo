abba1611151082166b9da77e9eade191046a5038
prageeth <prageeth.madhu@gmail.com>
2018-08-12 10:02:46 +0530

    add modules

--- evergo/src/app/app.module.ts
+++ evergo/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FreequentlyAskQuestionsComponent } from './freequently-ask-questions/freequently-ask-questions.component';
+import { MainCarousalComponent } from './main-carousal/main-carousal.component';
+import { SubCarouselComponent } from './sub-carousel/sub-carousel.component';
+import { NewsEventComponent } from './news-event/news-event.component';
+import { TourDetailComponent } from './tour-detail/tour-detail.component';
+import { TourTestimonialComponent } from './tour-testimonial/tour-testimonial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavigationComponent
+    TopNavigationComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    FreequentlyAskQuestionsComponent,
+    MainCarousalComponent,
+    SubCarouselComponent,
+    NewsEventComponent,
+    TourDetailComponent,
+    TourTestimonialComponent
   ],
   imports: [
     BrowserModule,
---
+++ evergo/src/app/main-carousal/main-carousal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carousal',
+  templateUrl: './main-carousal.component.html',
+  styleUrls: ['./main-carousal.component.css']
+})
+export class MainCarousalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
